fa81779500f6fd9017edc6be62db87814c3dc766
Jan Fooken <git@bode.fun>
2023-08-07 09:13:04 +0200

    feat: Add custom error handling
    Handle usage and help request as well and other errors in a separate function to ensue deferred functions can run.
    
    Add a custom printUsage function and disable pflag's built-in one

--- cmd/xff/main.go
+++ cmd/xff/main.go
@@ -1,6 +1,9 @@
+// TODO: Setup golangci to lint
+// TODO: Wrap errors
 package main
 
 import (
+	"errors"
 	"fmt"
 	"io"
 	"os"
@@ -9,16 +12,55 @@ import (
 	"github.com/spf13/pflag"
 )
 
+// TODO: Add custom exit codes depending on error
+var (
+	ErrHelpRequested error = errors.New("xff: help requested")
+	ErrUsage               = errors.New("xff: wrong usage")
+)
+
 type FileBuffer struct {
 	Path  string
 	Value []byte
 	File  *os.File
 }
 
+// Only pass exit codes between 0 and 125
+func printUsage(w io.Writer, exitCode uint, appName string, flag *pflag.FlagSet) {
+	options := flag.FlagUsages()
+	fmt.Fprintf(
+		w,
+		`usage:  %s [options] file
+
+options:
+%s`,
+		appName,
+		options,
+	)
+	os.Exit(int(exitCode))
+}
+
+// Do not defer functions in this proc, because they will not be executed, due to os.Exit()
 func main() {
 	appName := filepath.Base(os.Args[0])
 
-	flag := pflag.NewFlagSet(appName, pflag.ExitOnError)
+	// TODO: Add config struct and pull out flag config
+	flag := pflag.NewFlagSet(appName, pflag.ContinueOnError)
+
+	if err := mainE(appName, flag); err != nil {
+		if errors.Is(err, ErrHelpRequested) {
+			printUsage(os.Stdout, 0, appName, flag)
+		} else if errors.Is(err, ErrUsage) {
+			printUsage(os.Stderr, 1, appName, flag)
+		} else {
+			fmt.Fprintln(os.Stderr, err)
+		}
+	} else {
+		os.Exit(0)
+	}
+}
+
+// Do not call os.Exit in this function, otherwise deferred functions will not execute!
+func mainE(appName string, flag *pflag.FlagSet) error {
 
 	// Define flags
 	// ---------------------------------------------------
@@ -29,35 +71,39 @@ func main() {
 	// Set flags
 	// ---------------------------------------------------
 
-	flag.Parse(os.Args)
+	// Make Usage a no-op, because I handle this on my own
+	flag.Usage = func() {}
+
+	if err := flag.Parse(os.Args); err != nil {
+		if errors.Is(err, pflag.ErrHelp) {
+			return ErrHelpRequested
+		} else {
+			return err
+		}
+	}
 
 	// ---------------------------------------------------
 
 	args := flag.Args()
 
 	if len(args) <= 1 {
-		fmt.Printf("usage %s: [options] file\n", appName)
-		os.Exit(1)
+		return ErrUsage
 	}
 
 	absFilePath, err := filepath.Abs(args[1])
 	if err != nil {
-		fmt.Fprintln(os.Stderr, err)
-		os.Exit(1)
+		return err
 	}
 
-	f, err := os.Open(absFilePath)
+	// FIXME: Maybe create it, when it does not exist
+	f, err := os.OpenFile(absFilePath, os.O_RDWR, 0)
 	if err != nil {
-		fmt.Fprintln(os.Stderr, err)
-		os.Exit(1)
+		return err
 	}
-
-	defer f.Close()
 
 	fileBytes, err := io.ReadAll(f)
 	if err != nil {
-		fmt.Fprintln(os.Stderr, err)
-		os.Exit(1)
+		return err
 	}
 
 	currentBuffer := &FileBuffer{
@@ -66,5 +112,9 @@ func main() {
 		File:  f,
 	}
 
+	defer currentBuffer.File.Close()
+
 	fmt.Printf("%d\n", currentBuffer.Value)
+
+	return nil
 }
